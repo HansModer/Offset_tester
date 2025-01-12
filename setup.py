6b1617118d345725c463e063494c29b18051239c
Hans_Moderator <hans.6794.sso@gmail.com>
2025-01-12 21:06:20 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='offset-tester',
+    version='1.0.0',
+    description='A library to test offsets in buffers and memory safely.',
+    author='Your Name',
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
